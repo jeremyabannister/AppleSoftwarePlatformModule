6a8e60d7fe5a0e5f317b8c8e23eeb3f90a72bb70
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-07-23 00:38:22 +0200

    Added `AppleSoftwarePlatform`.

---
+++ Sources/AppleSoftwarePlatformModule/apple_software_platform/AppleSoftwarePlatform.swift
@@ -0,0 +1,22 @@
+//
+//  AppleSoftwarePlatform.swift
+//  
+//
+//  Created by Jeremy Bannister on 7/23/22.
+//
+
+///
+public enum AppleSoftwarePlatform {
+    
+    ///
+    case macOS
+    
+    ///
+    case iOS
+    
+    ///
+    case watchOS
+    
+    ///
+    case tvOS
+}
