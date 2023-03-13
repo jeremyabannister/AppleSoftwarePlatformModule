bf8b170c8e4fe55ab797640bd9b9e45c8a0bbfb3
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-03-13 13:35:43 +0100

    Added static constant `AppleSoftwarePlatform.local` for all current platforms.

---
+++ Sources/AppleSoftwarePlatformModule/apple_software_platform/AppleSoftwarePlatform_local.swift
@@ -0,0 +1,21 @@
+//
+//  AppleSoftwarePlatform_local.swift
+//  
+//
+//  Created by Jeremy Bannister on 3/13/23.
+//
+
+///
+extension AppleSoftwarePlatform {
+    
+    ///
+    #if os(macOS)
+    public static let local: Self = .macOS
+    #elseif os(iOS)
+    public static let local: Self = .iOS
+    #elseif os(watchOS)
+    public static let local: Self = .watchOS
+    #elseif os(tvOS)
+    public static let local: Self = .tvOS
+    #endif
+}
